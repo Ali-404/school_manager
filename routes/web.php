140727a6c15dfafed8550ba515117804571091dc
KHALID <rick.darkness1996@gmail.com>
2025-11-21 19:30:21 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,24 @@ Route::get('/', function () {
 Route::middleware("auth")->get("/user", function () {
     return Auth()->user();
 });
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+
+Route::get('/student-login', function () {
+    return view('auth.student-login');
+})->name('student.login');
+
+Route::post('/student-login', function () {
+    return '  login !';
+});
+
